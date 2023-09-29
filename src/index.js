2471024c5ac528cf3af8f5854144697a112e9da8
LucasVeloz <lurucell@gmail.com>
2023-09-29 16:11:32 -0300

    chore: removing console

--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ const fs = require('fs')
 const { Worker } = require("worker_threads");
 
 const json = JSON.parse(fs.readFileSync(process.argv[2]))
-const worker = new Worker('./src/transpiler.js', { workerData: json.expression })
-
-worker.on("online", () => console.time("transpiler worker ->"))
-worker.on("exit", () => console.timeEnd("transpiler worker ->"))
+new Worker('./src/transpiler.js', { workerData: json.expression })
